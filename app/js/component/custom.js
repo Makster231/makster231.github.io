527e9ae765f51c06e959bf7d4704c16b5e3ee140
Максим <makster559553@gmail.com>
2020-11-09 11:16:55 +0300

    corrected portfolio images on mobiles / telegram link

--- app/js/component/custom.js
+++ app/js/component/custom.js
@@ -1,32 +1,34 @@
-(function ($, window, document) {
-  function gallery() {
-    $(".js_works-page--slider").slick({
-      infinite: true,
-      speed: 700,
-      slidesToShow: 3,
-      lazyLoad: "ondemand",
-      slidesToScroll: 1,
-      responsive: [
-        {
-          breakpoint: 991,
-          settings: {
-            slidesToShow:1,
-          }
-        },
-      ],
+(function($, window, document) {
+    function gallery() {
+        $(".js_works-page--slider").slick({
+            infinite: true,
+            speed: 700,
+            slidesToShow: 3,
+            lazyLoad: "ondemand",
+            slidesToScroll: 1,
+            responsive: [{
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }, ],
+        });
+    }
+    $(() => {
+        if (!$is_mobile_size) {
+            gallery();
+        } else {
+            $.each($(".js_page-works img"), function() {
+                $(this).attr("src", $(this).attr("data-lazy"));
+            })
+        }
+
+        // activate light version of page in IE
+        if (isIE || isEdge) {
+            $(".js_wrapper-page").addClass("js_light-version");
+        }
+
+        //show content after loaded page
+        $("body").css("opacity", "1");
     });
-  }
-  $(() => {
-    if (!$is_mobile_size) {
-      gallery();
-    }
-
-    // activate light version of page in IE
-    if (isIE || isEdge) {
-      $(".js_wrapper-page").addClass("js_light-version");
-    }
-
-    //show content after loaded page
-    $("body").css("opacity", "1");
-  });
 })(window.jQuery, window, document);
