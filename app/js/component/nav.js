607020cee239d1c5afc56c12f7e6d342f43d63df
Максим <makster559553@gmail.com>
2020-11-10 11:18:06 +0300

    added swipe event

--- app/js/component/nav.js
+++ app/js/component/nav.js
@@ -1,48 +1,49 @@
-(function ($, window, document) {
-  function navPages() {
-    nav.item.click(function () {
-      let $this = $(this);
-      nav.item.removeClass("js_active");
-      $this.addClass("js_active");
+(function($, window, document) {
+    function navPages() {
+        nav.item.click(function() {
+            let $this = $(this);
+            nav.item.removeClass("js_active");
+            $this.addClass("js_active");
 
-      if ($this.is(nav.about)) {
+            if ($this.is(nav.about)) {
+                setHeight(pages.about, 10);
+                rotateToSection(0);
+                pages.about.addClass("js_animation-about");
+            } else {
+                pages.about.removeClass("js_animation-about");
+            }
+
+            if ($this.is(nav.works)) {
+                setHeight($(".js_works-page--slider"), 110);
+                rotateToSection(1);
+                pages.works.addClass("js_animation-works");
+            } else {
+                pages.works.removeClass("js_animation-works");
+            }
+
+            if ($this.is(nav.costs)) {
+                setHeight(pages.costs, 20);
+
+                rotateToSection(2);
+                pages.costs.addClass("js_animation-costs");
+            } else {
+                pages.costs.removeClass("js_animation-costs");
+            }
+
+            if ($this.is(nav.contacts)) {
+                setHeight(pages.contacts, 20);
+
+                rotateToSection(3);
+                pages.contacts.addClass("js_animation-contacts");
+            } else {
+                pages.contacts.removeClass("js_animation-contacts");
+            }
+        });
+    }
+
+
+    $(() => {
         setHeight(pages.about, 10);
-        rotateToSection(0);
-        pages.about.addClass("js_animation-about");
-      } else {
-        pages.about.removeClass("js_animation-about");
-      }
-
-      if ($this.is(nav.works)) {
-        setHeight($(".js_works-page--slider"), 110);
-        rotateToSection(1);
-        pages.works.addClass("js_animation-works");
-      } else {
-        pages.works.removeClass("js_animation-works");
-      }
-
-      if ($this.is(nav.costs)) {
-        setHeight(pages.costs, 20);
-
-        rotateToSection(2);
-        pages.costs.addClass("js_animation-costs");
-      } else {
-        pages.costs.removeClass("js_animation-costs");
-      }
-
-      if ($this.is(nav.contacts)) {
-        setHeight(pages.contacts, 20);
-
-        rotateToSection(3);
-        pages.contacts.addClass("js_animation-contacts");
-      } else {
-        pages.contacts.removeClass("js_animation-contacts");
-      }
+        navPages();
     });
-  }
-
-  $(() => {
-    setHeight(pages.about, 10);
-    navPages();
-  });
 })(window.jQuery, window, document);
--- app/js/component/page_swipe.js
+++ app/js/component/page_swipe.js
@@ -1,97 +1,84 @@
-// (function ($, window, document) {
-//   var tsX;
-//   var tsY;
-//   $(document).bind("touchstart", function (e) {
-//     console.log(e);
-//     tsX = e.originalEvent.touches[0].clientX;
-//     tsY = e.originalEvent.touches[0].clientY;
-//   });
+(function($, window, document) {
 
-//   $(document).bind("touchend", function (e) {
-//     var teX = e.originalEvent.changedTouches[0].clientX;
-//     var teY = e.originalEvent.changedTouches[0].clientY;
+    $("#js_body").swipe({
+        //Generic swipe handler for all directions
+        swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+            console.log(direction);
 
-//     // tsX = Math.floor(tsX);
-//     // tsY = Math.floor(tsY);
-//     // teX = Math.floor(teX);
-//     // teY = Math.floor(teY);
+            pages.about.removeClass("js_animation-about");
+            pages.works.removeClass("js_animation-works");
+            pages.costs.removeClass("js_animation-costs");
+            pages.contacts.removeClass("js_animation-contacts");
 
-//     pages.about.removeClass("js_animation-about");
-//     pages.works.removeClass("js_animation-works");
-//     pages.costs.removeClass("js_animation-costs");
-//     pages.contacts.removeClass("js_animation-contacts");
+            if (direction === "left") {
+                if (nav.about.hasClass("js_active")) {
+                    rotateToSection(1);
+                    nav.about.removeClass("js_active");
+                    nav.works.addClass("js_active");
+                    pages.works.addClass("js_animation-works");
+                    return;
+                }
 
-//     console.log(e);
-//     console.log("tsX: "+tsX);
-//     console.log("tsY: "+tsY);
-//     console.log("teX: "+teX);
-//     console.log("teY: "+teY);
+                if (nav.works.hasClass("js_active")) {
+                    rotateToSection(2);
+                    nav.works.removeClass("js_active");
+                    nav.costs.addClass("js_active");
+                    pages.costs.addClass("js_animation-costs");
+                    return;
+                }
 
-//     if (tsX < teX) {
-//       if (nav.about.hasClass("js_active")) {
-//         rotateToSection(3);
-//         nav.about.removeClass("js_active");
-//         nav.contacts.addClass("js_active");
-//         pages.contacts.addClass("js_animation-contacts");
-//         return;
-//       }
+                if (nav.costs.hasClass("js_active")) {
+                    rotateToSection(3);
+                    nav.costs.removeClass("js_active");
+                    nav.contacts.addClass("js_active");
+                    pages.contacts.addClass("js_animation-contacts");
+                    return;
+                }
 
-//       if (nav.works.hasClass("js_active")) {
-//         rotateToSection(0);
-//         nav.works.removeClass("js_active");
-//         nav.about.addClass("js_active");
-//         pages.about.addClass("js_animation-about");
-//         return;
-//       }
+                if (nav.contacts.hasClass("js_active")) {
+                    rotateToSection(0);
+                    nav.contacts.removeClass("js_active");
+                    nav.about.addClass("js_active");
+                    pages.about.addClass("js_animation-about");
+                    return;
+                }
+            } else {
+                if (nav.about.hasClass("js_active")) {
+                    rotateToSection(3);
+                    nav.about.removeClass("js_active");
+                    nav.contacts.addClass("js_active");
+                    pages.contacts.addClass("js_animation-contacts");
+                    return;
+                }
 
-//       if (nav.costs.hasClass("js_active")) {
-//         rotateToSection(1);
-//         nav.costs.removeClass("js_active");
-//         nav.works.addClass("js_active");
-//         pages.works.addClass("js_animation-works");
-//         return;
-//       }
+                if (nav.works.hasClass("js_active")) {
+                    rotateToSection(0);
+                    nav.works.removeClass("js_active");
+                    nav.about.addClass("js_active");
+                    pages.about.addClass("js_animation-about");
+                    return;
+                }
 
-//       if (nav.contacts.hasClass("js_active")) {
-//         rotateToSection(2);
-//         nav.contacts.removeClass("js_active");
-//         nav.costs.addClass("js_active");
-//         pages.costs.addClass("js_animation-costs");
-//         return;
-//       }
-//     } else {
-//       if (nav.about.hasClass("js_active")) {
-//         rotateToSection(1);
-//         nav.about.removeClass("js_active");
-//         nav.works.addClass("js_active");
-//         pages.works.addClass("js_animation-works");
-//         return;
-//       }
+                if (nav.costs.hasClass("js_active")) {
+                    rotateToSection(1);
+                    nav.costs.removeClass("js_active");
+                    nav.works.addClass("js_active");
+                    pages.works.addClass("js_animation-works");
+                    return;
+                }
 
-//       if (nav.works.hasClass("js_active")) {
-//         rotateToSection(2);
-//         nav.works.removeClass("js_active");
-//         nav.costs.addClass("js_active");
-//         pages.costs.addClass("js_animation-costs");
-//         return;
-//       }
+                if (nav.contacts.hasClass("js_active")) {
+                    rotateToSection(2);
+                    nav.contacts.removeClass("js_active");
+                    nav.costs.addClass("js_active");
+                    pages.costs.addClass("js_animation-costs");
+                    return;
+                }
+            }
 
-//       if (nav.costs.hasClass("js_active")) {
-//         rotateToSection(3);
-//         nav.costs.removeClass("js_active");
-//         nav.contacts.addClass("js_active");
-//         pages.contacts.addClass("js_animation-contacts");
-//         return;
-//       }
 
-//       if (nav.contacts.hasClass("js_active")) {
-//         rotateToSection(0);
-//         nav.contacts.removeClass("js_active");
-//         nav.about.addClass("js_active");
-//         pages.about.addClass("js_animation-about");
-//         return;
-//       }
-//     }
-//   });
-//   $(() => {});
-// })(window.jQuery, window, document);
+        }
+    });
+
+    $(() => {});
+})(window.jQuery, window, document);
